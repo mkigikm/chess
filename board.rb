97f7e25013ce448966af0a47d76112af45914a53
Chris + Matt <chris+matt@appacademy.io>
2015-01-27 10:32:36 -0500

    Create class files and preliminary initialize Board class.

---
+++ board.rb
@@ -0,0 +1,9 @@
+class Board
+
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) {nil}}
+  end
+  
+end
---
+++ sliding_piece.rb
@@ -0,0 +1,4 @@
+require_relative 'piece.rb'
+
+class SlidingPiece
+end
