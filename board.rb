3d8c2655628b78992d332e9e35436d7d4fd633f2
Chris + Matt <chris+matt@appacademy.io>
2015-01-27 10:40:20 -0500

    added generic piece placement

--- board.rb
+++ board.rb
@@ -5,5 +5,13 @@ class Board
   def initialize
     @grid = Array.new(8) { Array.new(8) {nil}}
   end
-  
+
+  def []=(pos, piece)
+    @grid[pos[0]][pos[1]] = piece
+  end
+
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
 end
--- piece.rb
+++ piece.rb
@@ -1,4 +1,20 @@
 require_relative 'board.rb'
 
 class Piece
+
+  attr_reader :color
+
+  def initialize(color, pos)
+    @color = color
+    @pos = pos
+  end
+
+  def place(board)
+    @board = board
+    board[@pos] = self
+  end
+
+  def moves
+    raise NotImplementedError
+  end
 end
